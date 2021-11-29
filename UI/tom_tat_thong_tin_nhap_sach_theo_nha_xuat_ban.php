ba1f802c835ccdeeff02cba838afebd54ab7b230
thanhtoan1742 <thanhtoan1742@gmail.com>
2021-11-29 14:23:05 +0700

    2 trang data

--- UI/tom_tat_thong_tin_nhap_sach_theo_nha_xuat_ban.php
+++ UI/tom_tat_thong_tin_nhap_sach_theo_nha_xuat_ban.php
@@ -1,31 +1,8 @@
+
 <?php
 include_once('init.php');
 
-if (isset($_POST["btn-del"])) {
-  $ma = $_POST["ma"];
-  $query = "DELETE FROM nhan_vien WHERE ma ='" . $ma . "';";
-  $conn->query($query);
-}
-
-
-$query = "SELECT "
-  . "    nhan_vien.ma, "
-  . "    nhan_vien.ho, "
-  . "    nhan_vien.ten, "
-  . "    nhan_vien.cccd, "
-  . "    nhan_vien.ngay_sinh, "
-  . "    nhan_vien.email, "
-  . "    nhan_vien.sdt, "
-  . "    nhan_vien.dia_chi, "
-  . "    nhan_vien.thoi_gian_bat_dau_lam, "
-  . "    nhan_vien.luong, "
-  . "    chi_nhanh.ten AS ten_chi_nhanh "
-  . "FROM "
-  . "    nhan_vien, chi_nhanh "
-  . "WHERE "
-  . "    nhan_vien.ma_chi_nhanh = chi_nhanh.ma "
-  . "ORDER BY "
-  . "    chi_nhanh.ma; ";
+$query = 'CALL tom_tat_thong_tin_nhap_sach_theo_nha_xuat_ban()';
 $result = $conn->query($query);
 if ($result->num_rows > 0) {
   $col = 1;
@@ -33,27 +10,10 @@ if ($result->num_rows > 0) {
   while ($row = $result->fetch_assoc()) {
     $data .= '<tr>
         <td scope="col">' . $col . '</td>
-        <td scope="col">' . $row["ho"] . '</td>
         <td scope="col">' . $row["ten"] . '</td>
-        <td scope="col">' . $row["cccd"] . '</td>
-        <td scope="col">' . $row["ngay_sinh"] . '</td>
-        <td scope="col">' . $row["email"] . '</td>
-        <td scope="col">' . $row["sdt"] . '</td>
-        <td scope="col">' . $row["dia_chi"] . '</td>
-        <td scope="col">' . $row["thoi_gian_bat_dau_lam"] . '</td>
-        <td scope="col">' . $row["luong"] . '</td>
-        <td scope="col">' . $row["ten_chi_nhanh"] . '</td>
+        <td scope="col">' . $row["so_luong_sach"] . '</td>
+        <td scope="col">' . $row["tien_tra_nha_xuat_ban"] . '</td>
         <td scope="col">
-        <form action="sua_nhan_vien.php" method="GET">
-            <input type="text" value="' . $row["ma"] . '" name ="ma" hidden>
-            <button class="btn btn-secondary">Sửa</button></td>
-        </form>
-        <td scope="col">
-          <form action="" method="POST">
-            <input type="text" value="' . $row["ma"] . '" name ="ma" hidden>
-            <button class="btn btn-success" type="submit" name="btn-del">Xóa</button>
-          </form>
-        </td>
       </tr>';
     $col++;
   }
@@ -94,13 +54,7 @@ $conn->close();
         </ul>
         <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
           <li class="nav-item" style="margin-right: 10px;">
-            <a href="tom_tat_thong_tin_nhap_sach_theo_nha_xuat_ban.php" class="btn btn-secondary ">Tóm tắt thông tin nhập sách theo nhà xuất bản</a>
-          </li>
-          <li class="nav-item" style="margin-right: 10px;">
             <a href="doanh_thu_chi_nhanh.php" class="btn btn-secondary ">Doanh thu chi nhánh</a>
-          </li>
-          <li class="nav-item">
-            <a href="them_nhan_vien.php" class="btn btn-primary ">Thêm nhân viên mới</a>
           </li>
         </ul>
       </div>
@@ -113,16 +67,9 @@ $conn->close();
       <thead>
         <tr>
           <th scope="col">STT</th>
-          <th scope="col">Họ </th>
           <th scope="col">Tên</th>
-          <th scope="col">CCCD</th>
-          <th scope="col">Ngày sinh</th>
-          <th scope="col">Email</th>
-          <th scope="col">Số điện thoại</th>
-          <th scope="col">Địa Chỉ</th>
-          <th scope="col">Thời gian bắt đầu làm</th>
-          <th scope="col">Lương</th>
-          <th scope="col">Tên chi nhánh</th>
+          <th scope="col">Số lượng sách</th>
+          <th scope="col">Tiền trả nhà xuất bản</th>
         </tr>
       </thead>
       <tbody>
