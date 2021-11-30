f6bf7697dcfad61dc1a68f45c28d9c751d6eb556
thanhdat <thanhdat.dev.js@gmail.com>
2021-11-30 21:43:14 +0700

    fix

--- UI/get_data_1.php
+++ UI/get_data_1.php
@@ -47,7 +47,7 @@ $conn->close();
 <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="dau_sach.php" style="font-weight: 700;">Bảng đầu sách</a>
+      <a class="navbar-brand" href="dau_sach.php" style="font-weight: 700;">Bảng đầu sách (Đạt)</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
